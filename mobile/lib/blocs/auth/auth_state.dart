79ede50a0183bf7cd3201d61c87d03233123fafb
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-06-22 14:39:08 +0700

    definisikan status (state) autentikasi UI untuk loading, sukses, dan gagal

---
+++ mobile/lib/blocs/auth/auth_state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {}
+class AuthLoading extends AuthState {}
+class AuthAuthenticated extends AuthState {
+  final String userId;
+  const AuthAuthenticated(this.userId);
+
+  @override
+  List<Object?> get props => [userId];
+}
+class AuthUnauthenticated extends AuthState {}
+class AuthRegisterSuccess extends AuthState {}
+class AuthError extends AuthState {
+  final String message;
+  const AuthError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
+
+class AuthGuestMode extends AuthState {}
