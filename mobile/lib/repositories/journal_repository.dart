1c5bd3204a306e7ffd9bc5c4f7ab2511c2ccf4de
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-06-22 14:49:49 +0700

    buat lapisan repositori data untuk komunikasi API dan database SQLite

---
+++ mobile/lib/repositories/journal_repository.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import '../services/api_client.dart';
+
+class JournalRepository {
+  final Dio _dio = ApiClient.instance;
+
+  Future<Response> getDrafts() async {
+    return await _dio.get('/journals/drafts');
+  }
+
+  Future<Response> createJournal(FormData data) async {
+    return await _dio.post('/journals', data: data);
+  }
+
+  Future<Response> updateJournal(String id, FormData data) async {
+    return await _dio.patch('/journals/$id', data: data);
+  }
+
+  Future<Response> publishJournal(String id) async {
+    return await _dio.patch('/journals/$id/publish');
+  }
+
+  Future<Response> archiveJournal(String id) async {
+    return await _dio.patch('/journals/$id/archive');
+  }
+
+  Future<Response> deleteJournal(String id) async {
+    return await _dio.delete('/journals/$id');
+  }
+
+  Future<Response> syncJournals(List<dynamic> draftsToSync) async {
+    return await _dio.post(
+      '/journals/sync',
+      data: {'drafts': draftsToSync},
+    );
+  }
+
+  Future<Response> addBookmark(String id) async {
+    return await _dio.post('/bookmarks', data: {'journalId': id});
+  }
+}
